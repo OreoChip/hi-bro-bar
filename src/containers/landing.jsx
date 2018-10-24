bd4156671e6908d4c4154e10c0cbea86eb8a3df6
tushar0837 <tushar0837@gmail.com>
2018-10-25 01:38:32 +0530

    added about us text

--- src/containers/landing.jsx
+++ src/containers/landing.jsx
@@ -15,6 +15,7 @@ import { container } from 'design-system/assets/jss/material-kit-react.jsx';
 
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
+import Divider from '@material-ui/core/Divider';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -57,7 +58,7 @@ const styles = () => ({
   },
   fontColor: {
     color: 'black',
-    padding: "20px 0px",
+    padding: "10px 0px",
     textAlign: 'center'
   },
   fontColor1: {
@@ -71,9 +72,9 @@ const styles = () => ({
   },
   card: {
     width: "30%",
-    margin: "3%" 
+    margin: "3%"
   },
-  cardsContainer:{
+  cardsContainer: {
     display: "flex",
     flexDirection: "row"
   }
@@ -104,9 +105,20 @@ class Landing extends Component {
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div className={classNames(classes.container)}>
             <h2 className={classes.fontColor}> Welcome to Hi Brow Bar Threading Salon</h2>
-            <Typography className={classes.fontColor1} variant="h5">
+            <Typography className={classes.fontColor1} variant="h6">
               We are offering you reasonable and best service for eye brow threading, facial & wax. You should visit any of our salons one time you will surely come next time, our experienced staff provides you 100% satisfaction, its our main goal to satisfy each and every customer whether its about eye brow threading, facial or waxing. You can contact us on any of the above numebers. We are known for excellent service at affordable price, we appreciate your interest in our service. We are serving two locations Falls Church and Alexendria.
             </Typography>
+            <Divider />
+            <h2 className={classes.fontColor}> About Us</h2>
+            <div className={classes.cardsContainer}>
+            </div>
+            <Typography className={classes.fontColor} variant="h6">
+            Hi Brow Bar is the leading salon serving two locations Falls Church and Alexandria Virginia, We are precisely serving eye brow threading, facial and waxing. We have experienced team with more then 10 years of experience. Our staff has gained valuable image over our precious customers, who mainly focus to provide 100% satisfaction.
+            <br/><br/>
+            We are serving fairfax, falls church, alexandria, arlington and other nearby customers everyday. All the members of our staff are well qualified, we can't wait to see you at our salon. If you have further querries or questions then you can call us @ (703) 618-4464 or any of above store numbers.
+            </Typography>
+
+            <Divider/>
             <h2 className={classes.fontColor}> Our Services</h2>
             <div className={classes.cardsContainer}>
               <Card className={classes.card}>
@@ -118,10 +130,10 @@ class Landing extends Component {
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                    Eye Brow Threading
+                      Eye Brow Threading
                   </Typography>
                     <Typography component="p">
-                    We recommend eyebrow threading as the best way to shape, trim and groom eyebrows. You get sharp, picture perfect eyebrows as a result.
+                      We recommend eyebrow threading as the best way to shape, trim and groom eyebrows. You get sharp, picture perfect eyebrows as a result.
                   </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -141,10 +153,10 @@ class Landing extends Component {
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                    Waxing
+                      Waxing
                   </Typography>
                     <Typography component="p">
-                    There’s a ton of beauty salons that provide waxing services, yet our standards can be felt and seen from the moment you enter. 
+                      There’s a ton of beauty salons that provide waxing services, yet our standards can be felt and seen from the moment you enter.
                   </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -167,7 +179,7 @@ class Landing extends Component {
                       Facial
                   </Typography>
                     <Typography component="p">
-                    Looking for an everyday facial without the need to overpay? BTW, we only serve women so you can relax in full privacy. You can book an appointment right away.
+                      Looking for an everyday facial without the need to overpay? BTW, we only serve women so you can relax in full privacy. You can book an appointment right away.
                   </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -177,7 +189,11 @@ class Landing extends Component {
                 </Button>
                 </CardActions>
               </Card>
-            </div>
+            </div>x
+            <Typography className={classes.fontColor} variant="h6">
+              If you need special assistance of any kind please feel free to call us any of the above numbers. We are offering special discounts for regular customers. To avail our special services please visit our any covenient location either Falls Church or Alexandria VA.
+            </Typography>
+            <Divider />
           </div>
         </div>
       </div >
