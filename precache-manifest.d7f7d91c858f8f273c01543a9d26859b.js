4a95495ead2d1c46b6de607c3b51336d0fd5f008
psr <prabhjotsinghrai1@gmail.com>
2018-10-26 20:29:30 +0530

    Updates

---
+++ precache-manifest.d7f7d91c858f8f273c01543a9d26859b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfea2aa32486ed75b3a0cc2ea74242a",
+    "url": "/hi-bro-bar/static/media/image3.7dfea2aa.jpg"
+  },
+  {
+    "revision": "e41c6993a82297e421b50e9f7ee1965e",
+    "url": "/hi-bro-bar/static/media/image2.e41c6993.jpg"
+  },
+  {
+    "revision": "a4f82040d13219d74aa688293ac9faeb",
+    "url": "/hi-bro-bar/static/media/barber.a4f82040.jpg"
+  },
+  {
+    "revision": "1dbba8adfbf7e01a0d49",
+    "url": "/hi-bro-bar/static/js/runtime~main.1dbba8ad.js"
+  },
+  {
+    "revision": "1559a55b1357c5a64dc8",
+    "url": "/hi-bro-bar/static/js/main.1559a55b.chunk.js"
+  },
+  {
+    "revision": "01a76328c280d464c947",
+    "url": "/hi-bro-bar/static/js/1.01a76328.chunk.js"
+  },
+  {
+    "revision": "1559a55b1357c5a64dc8",
+    "url": "/hi-bro-bar/static/css/main.dcb3de9c.chunk.css"
+  },
+  {
+    "revision": "e4c8c8c9d083765a579e21002e99b6cc",
+    "url": "/hi-bro-bar/index.html"
+  }
+];
