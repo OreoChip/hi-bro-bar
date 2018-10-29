ce6aab30c7d52e6e09d1efed04fa64224a835060
psr <prabhjotsinghrai1@gmail.com>
2018-10-30 00:21:45 +0530

    feat: falls church and Alexandria

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -22,6 +22,7 @@ const locations = [
     iframe: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3107.6228186356466!2d-77.11888842822806!3d38.84109818124534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b3ee7e444e15%3A0x467faf683aa41f4a!2s3817+S+George+Mason+Dr%2C+Falls+Church%2C+VA+22041%2C+USA!5e0!3m2!1sen!2sin!4v1540838605864"
+        title={'falls church'}
         frameBorder="0"
         style={{ border: 0, width: '100%', height: '100%' }}
         allowFullScreen
@@ -44,8 +45,7 @@ const locations = [
     iframe: (
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3109.139142481263!2d-77.1340592845694!3d38.806364979584735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b24f28154e2f%3A0x24a52602a720f3b1!2s510+S+Van+Dorn+St%2C+Alexandria%2C+VA+22304%2C+USA!5e0!3m2!1sen!2sin!4v1540838749798"
-        width="600"
-        height="450"
+        title={'alexandria'}
         frameBorder="0"
         style={{ border: 0, width: '100%', height: '100%' }}
         allowFullScreen
