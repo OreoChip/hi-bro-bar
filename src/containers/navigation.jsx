358d7094979e1911b1f64fbc57c5f0f704211d22
tushar0837 <tushar0837@gmail.com>
2018-10-31 02:51:55 +0530

    updated brand with node instead of string for more flexibility

--- src/containers/navigation.jsx
+++ src/containers/navigation.jsx
@@ -3,6 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import PubSub from 'pubsub-js';
+import classNames from "classnames";
 
 import Header from 'design-system/components/Header/Header.jsx';
 import Button from 'design-system/components/CustomButtons/Button.jsx';
@@ -45,12 +46,22 @@ class Navigation extends Component {
       </ListItem>
     );
   };
+  renderBrand = (classes) => {
+    return (
+    <div className={classes.brandContainter}>
+      <div className={classes.mainText}>Hi Brow Bar</div>
+      <div className={classes.subText}>Threading, Facial, & Waxing Salon</div>
+    </div>
+    )
+  }
 
   render() {
     const { classes } = this.props;
     return (
       <Header
-        brand={'Hi Brow Bar'}
+        brand={
+          this.renderBrand(classes)
+        }
         color="white"
         fixed
         bioRhyme
