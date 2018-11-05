7f3d36c9448152d7ce11629d7071ccfb52f559bd
Abhas Mittal <mittalabhas1@gmail.com>
2018-11-05 14:06:14 +0530

    make work on IE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'es6-shim';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import RouterContainer from './router';
