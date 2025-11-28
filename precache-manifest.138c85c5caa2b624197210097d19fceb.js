592c212092a727b800c46ed6d23619a50e1a1bfc
Prabhjot Singh Rai <hi@psr.ai>
2025-11-29 02:47:18 +0530

    Updates

---
+++ precache-manifest.138c85c5caa2b624197210097d19fceb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ec2b2d2012b7386bcd0",
+    "url": "/static/css/main.1c7ac960.chunk.css"
+  },
+  {
+    "revision": "8ec2b2d2012b7386bcd0",
+    "url": "/static/js/main.8ec2b2d2.chunk.js"
+  },
+  {
+    "revision": "59b9d44738ddc0e109e3",
+    "url": "/static/css/1.e2e65ba4.chunk.css"
+  },
+  {
+    "revision": "59b9d44738ddc0e109e3",
+    "url": "/static/js/1.59b9d447.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62ad52f83200e4bf0a2126be2e1244ca",
+    "url": "/static/media/threading.62ad52f8.jpg"
+  },
+  {
+    "revision": "5b470b4dda9554047eddbf79e38ce7de",
+    "url": "/static/media/wax.5b470b4d.jpg"
+  },
+  {
+    "revision": "d6aae47259d9cd0a642075b210b5bd1e",
+    "url": "/static/media/facial.d6aae472.jpg"
+  },
+  {
+    "revision": "5c9793743580dbcea62fceaa20798c6b",
+    "url": "/static/media/image1.5c979374.jpg"
+  },
+  {
+    "revision": "13d9ca48ec321d58d6c21d41340d64ba",
+    "url": "/static/media/image2.13d9ca48.jpg"
+  },
+  {
+    "revision": "8e8edbd611578a85a5cd1bcfc75a3b1f",
+    "url": "/static/media/image3.8e8edbd6.jpg"
+  },
+  {
+    "revision": "a64eb92944a3bcbcb0d878facb678e30",
+    "url": "/static/media/image1.a64eb929.jpg"
+  },
+  {
+    "revision": "33f6b8395338918c4a6c9cbb0fb7efa2",
+    "url": "/static/media/image2.33f6b839.jpg"
+  },
+  {
+    "revision": "1cd576d391f58fa432d43440ea871f20",
+    "url": "/static/media/image3.1cd576d3.jpg"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "f5af56ee3b8af74bd76bd4595a903184",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a80798971e26781a7e5d5d051da01d2.js"
+  "/precache-manifest.138c85c5caa2b624197210097d19fceb.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
